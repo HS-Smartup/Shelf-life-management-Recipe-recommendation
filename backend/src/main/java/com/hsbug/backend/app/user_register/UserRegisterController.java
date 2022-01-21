d42fb2825acaa3956afed4b438f5c5a3c5154da6
jungking <immenige2@gmail.com>
2022-01-21 17:54:00 +0900

    fix :  json-simple 추가

--- backend/src/main/java/com/hsbug/backend/app/user_register/UserRegisterController.java
+++ backend/src/main/java/com/hsbug/backend/app/user_register/UserRegisterController.java
@@ -1,10 +1,6 @@
 package com.hsbug.backend.app.user_register;
 
-<<<<<<< HEAD
 import org.json.simple.JSONObject;
-=======
-import org.h2.engine.User;
->>>>>>> 7952ad87a74ecf5f2f4122308156604443277373
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -44,24 +40,19 @@ public class UserRegisterController {
     }
 
     @PostMapping("/signup")     // 회원가입 post
-<<<<<<< HEAD
     public JSONObject CreateUser(@RequestBody UserRegisterDto userRegisterDto) {
         JSONObject obj = new JSONObject();
-=======
-    public String CreateUser(@RequestBody UserRegisterDto userRegisterDto) {
->>>>>>> 7952ad87a74ecf5f2f4122308156604443277373
         if (!userRegisterService.checkUserByUsername(userRegisterDto.getEmail())){
             System.out.println("이미 등록된 회원입니다.");
             obj.put("message","이미 등록된 회원입니다.");
-            return obj;
         }
         else {
             userRegisterService.save(userRegisterDto);           // service에 dto 저장
             System.out.println(userRegisterDto.getEmail());
             System.out.println(userRegisterDto.getPassword());
             obj.put("message","회원 가입 성공");
-            return obj;
         }
+        return obj;
     }
 }
 
