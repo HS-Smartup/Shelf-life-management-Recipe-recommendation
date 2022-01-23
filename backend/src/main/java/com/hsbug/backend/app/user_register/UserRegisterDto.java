a63b73030a38abf8a403a90a8d6d56a3fe477361
jungking <immenige2@gmail.com>
2022-01-23 15:04:22 +0900

    fix : 외부 로그인 저장

--- backend/src/main/java/com/hsbug/backend/app/user_register/UserRegisterDto.java
+++ backend/src/main/java/com/hsbug/backend/app/user_register/UserRegisterDto.java
@@ -15,53 +15,85 @@ import java.util.stream.Collectors;
 @Setter @Getter
 public class UserRegisterDto {
 
-    private Long id;        //id? 필요?
+    private Long id ;        //id? 필요?
     private String email;
     private String name;
     private String password;
     private String roles;
-    private String goofleSub; //구글 서브 용
+    private String google_sub; //구글 서브 용
     private String photo;     //프로필 사진 받는용
+    private String kakao_sub;
+    private String naver_sub;
+
 
     @Builder
-    public UserRegisterDto(Long id, String email, String name, String password, String roles, String goofleSub, String photo, String credit_check){
+    public UserRegisterDto(Long id, String email, String name, String password, String roles, String google_sub, String photo, String credit_check
+                            , String kakao_sub, String naver_sub){
         this.id = id;
         this.email = email;
         this.password = "{bcrypt}"+password;
         this.roles = "ROLE_USER";
 
         this.name = name;
-        this.goofleSub = goofleSub;
+        this.google_sub = google_sub;
         this.photo = photo;
+        this.kakao_sub = kakao_sub;
+        this.naver_sub = naver_sub;
     }
 
     /**
      * 구글 회원에게서 가져오는 폼중에서 필요한것만 따로 떨굼   (빌더회피를 위함)
      * @param email
      * @param name
-     * @param goofleSub
+     * @param google_sub
      * @param photo
      * password값을 입력하지 않으면 저장시 오류가 나는 경우가 있어서 google입력은 google이라는 값으로 통일했다.
      */
-    public void googleDtoOption(String email, String name, String goofleSub, String photo) {
+    public void googleDtoOption(String email, String name, String google_sub, String photo) {
         this.email = email;
         this.name = name;
-        this.goofleSub = goofleSub;
+        this.google_sub = google_sub;
         this.photo = photo;
-        this.password = "google";
+        this.password = "password";
+    }
+    public void kakaoDtoOption(String email, String name, String kakao_sub, String photo) {
+        this.email = email;
+        this.name = name;
+        this.kakao_sub = kakao_sub;
+        this.photo = photo;
+        this.password = "password";
     }
 
+    public void naverDtoOption(String email, String name, String naver_sub, String photo) {
+        this.email = email;
+        this.name = name;
+        this.naver_sub = naver_sub;
+        this.photo = photo;
+        this.password = "password";
+    }
 
+    public void clear() {
+        this.id =null;        //id? 필요?
+        this.email =null;
+        this.name=null;
+        this.password=null;
+        this.roles=null;
+        this.google_sub=null; //구글 서브 용
+        this.photo=null;     //프로필 사진 받는용
+        this.kakao_sub=null;
+        this.naver_sub=null;
+    }
 
     public UserRegisterEntity toEntity(){
         return UserRegisterEntity.builder()
-
                 .username(name)
                 .email(email)
                 .password(password)     // BCryptPasswordEncoder  == 스프링 시큐리티에서 제공, 비밀번호 암호화
                 .roles(roles)
-                .goofleSub(goofleSub)
+                .google_sub(google_sub)
                 .photo(photo)
+                .kakao_sub(kakao_sub)
+                .naver_sub(naver_sub)
                 .build();
     }
 
