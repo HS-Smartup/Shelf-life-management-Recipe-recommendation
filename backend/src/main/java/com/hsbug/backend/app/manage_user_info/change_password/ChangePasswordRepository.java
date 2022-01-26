455e1b7734027ee466ddb276b227aab765ab1a2c
jungking <immenige2@gmail.com>
2022-01-26 22:15:20 +0900

    feat : 비밀번호 변경 (인증은 아직)

---
+++ backend/src/main/java/com/hsbug/backend/app/manage_user_info/change_password/ChangePasswordRepository.java
@@ -0,0 +1,13 @@
+package com.hsbug.backend.app.manage_user_info.change_password;
+
+import com.hsbug.backend.app.user_register.UserRegisterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChangePasswordRepository extends JpaRepository<UserRegisterEntity,Long> {
+
+    Optional<UserRegisterEntity> findByEmail(String username);
+}
