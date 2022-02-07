cbedf6fd4025aaa6b972725db1448ed16b061d28
jungking <immenige2@gmail.com>
2022-02-07 15:30:35 +0900

    fixing : delete unused import

--- backend/src/main/java/com/hsbug/backend/app/manage_user_info/bookmark_recipe/BookmarkRecipeRepository.java
+++ backend/src/main/java/com/hsbug/backend/app/manage_user_info/bookmark_recipe/BookmarkRecipeRepository.java
@@ -1,10 +1,8 @@
 package com.hsbug.backend.app.manage_user_info.bookmark_recipe;
 
-import com.hsbug.backend.admin_page.manage_recipe.ManageRecipeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
