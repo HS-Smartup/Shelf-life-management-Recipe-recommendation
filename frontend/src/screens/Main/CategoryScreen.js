ac4b4376fd40cd66644a12783e3df5c323e9cd54
bear109 <jokerbear109@gmail.com>
2022-02-26 20:36:01 +0900

    feat: 기본 메인 페이지 추가

---
+++ frontend/src/screens/Main/CategoryScreen.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const CategoryScreen = () => {
+  return (
+    <View>
+      <Text>CategoryScreen</Text>
+    </View>
+  );
+};
+
+export default CategoryScreen;
+
+const styles = StyleSheet.create({});
---
+++ frontend/src/screens/Main/UserScreen.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const UserScreen = () => {
+  return (
+    <View>
+      <Text>UserScreen</Text>
+    </View>
+  );
+};
+
+export default UserScreen;
+
+const styles = StyleSheet.create({});
