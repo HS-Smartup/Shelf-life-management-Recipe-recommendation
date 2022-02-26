2c0b27a8315fd5e18f426783054d2c88736dc69a
bear109 <jokerbear109@gmail.com>
2022-02-26 20:35:42 +0900

    feat: bottom tab 구현

--- frontend/src/screens/Main/MainStack.js
+++ frontend/src/screens/Main/MainStack.js
@@ -1,11 +1,66 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from './HomeScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import CategoryScreen from './CategoryScreen';
+import RefrigeratorScreen from './RefrigeratorScreen';
+import UserScreen from './UserScreen';
+import SettingScreen from './SettingScreen';
+
+const Tab = createBottomTabNavigator();
 
 const MainStack = () => {
   return (
-    <View>
-      <Text>MainStack</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#ff8527',
+      }}>
+      <Tab.Screen
+        name="CategoryScreen"
+        component={CategoryScreen}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="list" size={32} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="RefrigeratorScreen"
+        component={RefrigeratorScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="kitchen" size={32} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="home" size={32} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="UserScreen"
+        component={UserScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="person" size={32} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SettingScreen"
+        component={SettingScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="settings" size={32} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
