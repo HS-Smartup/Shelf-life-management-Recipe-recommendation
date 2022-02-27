ea149d65b55969f1d277cab8c2e02404068d5e50
bear109 <jokerbear109@gmail.com>
2022-02-27 22:26:40 +0900

    feat: HomeScreen content 초기 구현

--- frontend/src/screens/Main/HomeScreen.js
+++ frontend/src/screens/Main/HomeScreen.js
@@ -8,23 +8,48 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const HomeScreen = ({navigation}) => {
-  const renderItem = () => (
-    <View>
-      <Pressable
-        style={styles.myRefrigerator}
-        onPress={() => console.log('hi')}>
-        <Image
-          source={require('../../assets/images/logo.png')}
-          style={styles.logo}
-        />
-        <Text style={styles.myRefrigeratorText}>내 냉장고 </Text>
-      </Pressable>
-    </View>
-  );
+  const [loading, setLoading] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
+  const [offset, setOffset] = useState(1);
+  const [isListEnd, setIsListEnd] = useState(false);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => getData(), []);
+
+  const getData = () => {
+    console.log(offset);
+    if (!loading && !isListEnd) {
+      console.log('getData');
+      setLoading(true);
+      // Service to get the data from the server to render
+      fetch('https://aboutreact.herokuapp.com/getpost.php?offset=' + offset)
+        // Sending the currect offset with get request
+        .then(response => response.json())
+        .then(responseJson => {
+          // Successful response from the API Call
+          console.log(responseJson);
+          if (responseJson.results.length > 0) {
+            setOffset(offset + 1);
+            // After the response increasing the offset
+            setDataSource([...dataSource, ...responseJson.results]);
+            setLoading(false);
+          } else {
+            setIsListEnd(true);
+            setLoading(false);
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  };
 
   return (
     <View style={styles.fullscreen}>
@@ -38,7 +63,7 @@ const HomeScreen = ({navigation}) => {
         </Pressable>
         <Pressable
           style={styles.searchWarpper}
-          onPress={() => console.log('hi')}>
+          onPress={() => navigation.navigate('SearchScreen')}>
           <View style={styles.search}>
             <Icon name="search" size={24} color={'#636773'} />
             <Text style={styles.searchText}>레시피 검색</Text>
@@ -52,7 +77,25 @@ const HomeScreen = ({navigation}) => {
         <FlatList
           style={styles.ScrollView}
           data={[{id: '1'}]}
-          renderItem={renderItem}
+          renderItem={() => (
+            <View>
+              <Pressable
+                style={styles.myRefrigerator}
+                onPress={() => console.log('hi')}>
+                <Image
+                  source={require('../../assets/images/logo.png')}
+                  style={styles.logo}
+                />
+                <Text style={styles.myRefrigeratorText}>내 냉장고 </Text>
+              </Pressable>
+              <FlatList
+                style={{backgroundColor: '#ffffff'}}
+                data={dataSource}
+                renderItem={({item}) => <Text>hi</Text>}
+              />
+            </View>
+          )}
+          keyExtractor={item => item.id.toString()}
         />
       </View>
     </View>
