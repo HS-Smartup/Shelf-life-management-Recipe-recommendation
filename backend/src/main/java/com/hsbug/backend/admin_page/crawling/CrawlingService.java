c522467aa847e4ea006e9a6f99e4406f6c401f62
hobae <bhb0407@naver.com>
2022-03-08 22:48:48 +0900

    feat : 웹 크롤링을 위한 서비스 생성 findText : 그냥 텍스트 가져오기 findTextByCssQuery : CssQuery으로 크롤링할 조건 선택이 가능함.

---
+++ backend/src/main/java/com/hsbug/backend/admin_page/crawling/CrawlingService.java
@@ -0,0 +1,38 @@
+package com.hsbug.backend.admin_page.crawling;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class CrawlingService {
+
+    //url 바탕으로 텍스트로 자료 가져오기
+    public String findText(String url) throws IOException {
+        Document docs = Jsoup.connect(url).get();
+        return docs.text();
+    }
+
+    public HashMap<String,List> findTextByCssQuery(CrawlingRequestDto requestDto) throws IOException {
+        HashMap<String, List> foodIngredientsMap = new HashMap<>();
+        List<String> searchList = new ArrayList<>();
+        Document docs = Jsoup.connect(requestDto.getUrl()).get();
+//        Elements elements = docs.select("h2[class=blog-shortcode-post-title entry-title]");
+        Elements elements = docs.select(requestDto.getCssQuery());
+
+
+        for (Element e : elements) {
+            searchList.add(e.text());
+        }
+
+        foodIngredientsMap.put("foodIngredients", searchList);
+        return foodIngredientsMap;
+    }
+}
