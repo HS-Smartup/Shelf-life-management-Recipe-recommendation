2b394425db55a6e5fa57f3933a9dcb2ded10eba2
jungking <immenige2@gmail.com>
2022-03-18 14:30:38 +0900

    append : classpath properties

--- backend/src/main/java/com/hsbug/backend/BackendApplication.java
+++ backend/src/main/java/com/hsbug/backend/BackendApplication.java
@@ -2,9 +2,11 @@ package com.hsbug.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:application-mysql.properties"})
 public class BackendApplication {
 
     public static void main(String[] args) {
