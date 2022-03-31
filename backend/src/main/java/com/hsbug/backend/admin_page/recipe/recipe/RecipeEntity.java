fd2b6cd6e1d0aad141c8673c36288960fc14ca88
hobae <bhb0407@naver.com>
2022-03-31 14:01:14 +0900

    fix : 프론트와 통신을 위해 만든 엔티티

---
+++ backend/src/main/java/com/hsbug/backend/admin_page/recipe/recipe/RecipeEntity.java
@@ -0,0 +1,29 @@
+package com.hsbug.backend.admin_page.recipe.recipe;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RECIPE_ID")
+    private Long id;
+    private String recipeName;
+    private int recipeNumber;
+    private String recipeWriter;
+    private String recipeMainImage;
+    private int recipeLikes;
+    private int recipeViews;
+    private float recipeStar; //recipeRationgs
+    private int recipeRatingCount;
+    private String recipeTime;
+    private String recipeLevel;
+    private String recipeServes;
+    private String recipeDescription;
+}
