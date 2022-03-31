048d72ad7c1f01a45dc1af63c6afd902adff4db6
hobae <bhb0407@naver.com>
2022-03-31 13:52:28 +0900

    fix : 레시피 테이블 테스트 변경하상

--- backend/src/test/java/com/hsbug/backend/admin_page/manage_recipe/ManageRecipeEntityTest.java
+++ backend/src/test/java/com/hsbug/backend/admin_page/manage_recipe/ManageRecipeEntityTest.java
@@ -1,7 +1,9 @@
 package com.hsbug.backend.admin_page.manage_recipe;
 
-import com.hsbug.backend.admin_page.recipe_attribute.RecipeIngredients;
-import com.hsbug.backend.admin_page.recipe_attribute.RecipeIngredientsRepository;
+import com.hsbug.backend.admin_page.recipe.recipe.RecipeEntity;
+import com.hsbug.backend.admin_page.recipe.recipe.RecipeRepository;
+import com.hsbug.backend.admin_page.recipe.recipe_attribute.RecipeIngredients;
+import com.hsbug.backend.admin_page.recipe.recipe_attribute.RecipeIngredientsRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,14 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ManageRecipeEntityTest {
 
     @Autowired
-    private ManageRecipeRepository recipeRepository;
+    private RecipeRepository recipeRepository;
     @Autowired
     private RecipeIngredientsRepository recipeIngredientsRepository;
 
     @Test
     public void test() {
-        ManageRecipeEntity recipeEntity = new ManageRecipeEntity();
-        recipeEntity.setINFO_NA("가ㅣ가ㅏ마ㅏ가가");
+        RecipeEntity recipeEntity = new RecipeEntity();
+        recipeEntity.setRecipeName("가ㅣ가ㅏ마ㅏ가가");
         recipeRepository.save(recipeEntity);
 
         RecipeIngredients recipeIngredients = new RecipeIngredients();
@@ -25,9 +27,9 @@ class ManageRecipeEntityTest {
 
         recipeIngredients.setRecipeEntityId(recipeEntity);
         recipeIngredientsRepository.save(recipeIngredients);
-        System.out.println("재료아이디: " + recipeIngredientsRepository.findByINGREDIENTSNAME("나나나나ㅏ나ㅏ난").getRecipeIngredientsId());
-        System.out.println("레시피 아이디: "+recipeIngredientsRepository.findByINGREDIENTSNAME("나나나나ㅏ나ㅏ난").getRecipeEntityId().getId());
-        System.out.println("레시피 이름"+recipeIngredientsRepository.findByINGREDIENTSNAME("나나나나ㅏ나ㅏ난").getIngredientName());
+//        System.out.println("재료아이디: " + recipeIngredientsRepository.findByINGREDIENTSNAME("나나나나ㅏ나ㅏ난").getRecipeIngredientsId());
+//        System.out.println("레시피 아이디: "+recipeIngredientsRepository.findByINGREDIENTSNAME("나나나나ㅏ나ㅏ난").getRecipeEntityId().getId());
+//        System.out.println("레시피 이름"+recipeIngredientsRepository.findByINGREDIENTSNAME("나나나나ㅏ나ㅏ난").getIngredientName());
 
     }
 
