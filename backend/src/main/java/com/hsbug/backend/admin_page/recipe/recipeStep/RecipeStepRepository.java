938a30198520dfca40aec4888a08e3a3237478ab
hobae <bhb0407@naver.com>
2022-04-01 21:15:29 +0900

    fix : 레시피아이디를 기준으로 검색하기 위한 레포지터리 검색만 생성

--- backend/src/main/java/com/hsbug/backend/admin_page/recipe/recipeStep/RecipeStepRepository.java
+++ backend/src/main/java/com/hsbug/backend/admin_page/recipe/recipeStep/RecipeStepRepository.java
@@ -3,6 +3,9 @@ package com.hsbug.backend.admin_page.recipe.recipeStep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeStepRepository extends JpaRepository<RecipeStepEntity, Long> {
+    List<RecipeStepEntity> findAllByRecipeEntityId(Long id);
 }
