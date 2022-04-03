407beea5fd31ddfaafcf9b455d61aa0210669762
hobae <bhb0407@naver.com>
2022-04-03 13:47:31 +0900

    fix : 한회원의 중복 평점을 방지하기 위해 컨트롤러 조건사항 생성

--- backend/src/main/java/com/hsbug/backend/admin_page/recipe/recipe_ratings/RecipeRatingController.java
+++ backend/src/main/java/com/hsbug/backend/admin_page/recipe/recipe_ratings/RecipeRatingController.java
@@ -2,7 +2,7 @@ package com.hsbug.backend.admin_page.recipe.recipe_ratings;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +15,17 @@ public class RecipeRatingController {
     private final RecipeRatingService recipeRatingService;
 
     @PostMapping("/add")
-    public HttpStatus addRating(@RequestBody RecipeRatingDto dto) {
-        log.info("recipe_id = {}", dto.getRecipeId());
-        recipeRatingService.saveRating(dto.toEntity());
-        recipeRatingService.ratingAverage(dto.getRecipeId());
-        return HttpStatus.OK;
+    public JSONObject addRating(@RequestBody RecipeRatingDto dto) {
+        JSONObject obj = new JSONObject();
+        boolean check = recipeRatingService.isAlreadyHasRecipe(dto);
+        if (check == false) {
+            recipeRatingService.saveRating(dto.toEntity());
+            recipeRatingService.ratingAverage(dto.getRecipeId());
+            obj.put("result", "새로운 평점 작성 완료");
+        } else {
+            recipeRatingService.editRating(dto);
+            obj.put("result", "기존의 평점이 변경되었습니다.");
+        }
+        return obj;
     }
 }
