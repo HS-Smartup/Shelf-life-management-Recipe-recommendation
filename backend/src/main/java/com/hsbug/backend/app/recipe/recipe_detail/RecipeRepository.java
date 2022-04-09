249df43495330d69e6f5bbc47693b9a4af24ae0a
hobae <bhb0407@naver.com>
2022-04-10 02:24:18 +0900

    fix : 카테고리 타입에 따를 레포지토리 메서드 타입 변경

--- backend/src/main/java/com/hsbug/backend/app/recipe/recipe_detail/RecipeRepository.java
+++ backend/src/main/java/com/hsbug/backend/app/recipe/recipe_detail/RecipeRepository.java
@@ -1,5 +1,9 @@
 package com.hsbug.backend.app.recipe.recipe_detail;
 
+import com.hsbug.backend.app.recipe.recipe_detail.category.IngredientsCategory;
+import com.hsbug.backend.app.recipe.recipe_detail.category.MethodCategory;
+import com.hsbug.backend.app.recipe.recipe_detail.category.SituationCategory;
+import com.hsbug.backend.app.recipe.recipe_detail.category.TypeCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,10 +13,10 @@ import java.util.Optional;
 @Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity,Long> {
     List<RecipeEntity> findAllByRecipeWriter(String email);
-    List<RecipeEntity> findAllByTypeCategory(String typeCategory);
-    List<RecipeEntity> findAllBySituationCategory(String situationCategory);
-    List<RecipeEntity> findAllByIngredientCategory(String ingredientCategory);
-    List<RecipeEntity> findAllByMethodCategory(String methodCategory);
+    List<RecipeEntity> findAllByTypeCategory(TypeCategory typeCategory);
+    List<RecipeEntity> findAllBySituationCategory(SituationCategory situationCategory);
+    List<RecipeEntity> findAllByIngredientCategory(IngredientsCategory ingredientCategory);
+    List<RecipeEntity> findAllByMethodCategory(MethodCategory methodCategory);
     List<RecipeEntity> findByRecipeNameContaining(String recipeName);
     Optional<RecipeEntity> findById(Long id);
 
