ef8ac6fd33307016e2a6a27462013d9bf35a313f
hobae <bhb0407@naver.com>
2022-04-11 18:07:17 +0900

    feat : admin계정 로그인 페이지 컨트롤러

--- backend/src/main/java/com/hsbug/backend/app/user_register/UserRegisterController.java
+++ backend/src/main/java/com/hsbug/backend/app/user_register/UserRegisterController.java
@@ -7,11 +7,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
-import java.net.http.HttpHeaders;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 /*
 
     로그인, 회원 가입, id/pw 찾기, 회원 탈퇴
@@ -27,19 +26,19 @@ public class UserRegisterController {
     private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/usercheck")
-    public JSONObject userCheck(@RequestHeader String token){
+    public JSONObject userCheck(@RequestHeader String token) {
         System.out.println(token);
         JSONObject obj = new JSONObject();
         boolean check = jwtTokenProvider.validateToken(token);
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        if (check){
-            obj.put("status",200);
-            obj.put("token",token);
+        if (check) {
+            obj.put("status", 200);
+            obj.put("token", token);
             obj.put("check", true);
             obj.put("user", email);
-        }else{
-            obj.put("status",200);
-            obj.put("token",token);
+        } else {
+            obj.put("status", 200);
+            obj.put("token", token);
             obj.put("check", false);
             obj.put("user", email);
         }
@@ -49,21 +48,22 @@ public class UserRegisterController {
     @GetMapping({"/loginSuccess", "/hello"})     // 로그인 성공시 get
     public JSONObject LoginsuccessPage() {
         JSONObject obj = new JSONObject();
-        obj.put("status",200);
+        obj.put("status", 200);
         obj.put("user email", SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+        obj.put("user name", SecurityContextHolder.getContext().getAuthentication().getName());
         return obj;
     }
 
     @GetMapping("/loginFailure")
-    public JSONObject LoginfailurePage(){
+    public JSONObject LoginfailurePage() {
         JSONObject obj = new JSONObject();
-        obj.put("message","로그인 실패");
-        obj.put("status",200);
+        obj.put("message", "로그인 실패");
+        obj.put("status", 200);
         return obj;
     }
 
     @GetMapping("/signup")     // 회원가입 페이지 Controller
-    public String SignupPage(){//@RequestBody AccountForm accountForm, HttpSession session) {
+    public String SignupPage() {//@RequestBody AccountForm accountForm, HttpSession session) {
 
         return "Sign up page";
     }
@@ -71,12 +71,11 @@ public class UserRegisterController {
     @PostMapping("/signup")     // 회원가입 post
     public JSONObject CreateUser(@RequestBody UserRegisterDto userRegisterDto) {
         JSONObject obj = new JSONObject();
-        if (!userRegisterService.checkUserByUsername(userRegisterDto.getEmail())){
+        if (!userRegisterService.checkUserByUsername(userRegisterDto.getEmail())) {
             System.out.println("이미 등록된 회원입니다.");
-            obj.put("status","???");
-            obj.put("message","이미 등록된 회원입니다.");
-        }
-        else {
+            obj.put("status", "???");
+            obj.put("message", "이미 등록된 회원입니다.");
+        } else {
             List<String> role = new ArrayList<>();
             role.add("ROLE_USER");
             userRegisterDto.setRoles(String.valueOf(role));
@@ -84,40 +83,42 @@ public class UserRegisterController {
             System.out.println(userRegisterDto.getEmail());
             System.out.println(userRegisterDto.getPassword());
             System.out.println(userRegisterDto.getRoles());
-            obj.put("message","회원 가입 성공");
-            obj.put("status",200);
+            obj.put("message", "회원 가입 성공");
+            obj.put("status", 200);
         }
         return obj;
     }
 
     @PostMapping("/signin")
-    public JSONObject login(@RequestBody Map<String, String> user){
-        List<String> role = new ArrayList<>();
-        role.add("ROLE_USER");
+    public JSONObject login(@RequestBody Map<String, String> user) {
+//        List<String> role = new ArrayList<>();
+//        role.add("ROLE_USER");
         JSONObject obj = new JSONObject();
 
-        if (userRegisterService.checkUserByUsername(user.get("email"))){
+        if (userRegisterService.checkUserByUsername(user.get("email"))) {
             obj.put("message", "잘못된 아이디입니다.");
-            obj.put("status","???");
-        }
-        else {
+            obj.put("status", "???");
+        } else {
             UserRegisterEntity member = userRegisterService.loadUserByUsername(user.get("email"));
 
             if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
                 obj.put("message", "잘못된 비밀번호입니다.");
-                obj.put("status","???");
-            }
-            else {
+                obj.put("status", "???");
+            } else {
                 obj.put("message", "로그인 성공");
-                obj.put("email",member.getEmail());
+                obj.put("email", member.getEmail());
                 //obj.put("id", member.getId().toString());
-                obj.put("status",200);
-                obj.put("token", jwtTokenProvider.createToken(user.get("email"), role));
+                obj.put("status", 200);
+                obj.put("token", jwtTokenProvider.createToken(user.get("email"), member.getRoles()));
                 obj.put("username", member.getUsername());
             }
         }
         return obj;
-
     }
+
+//    @PostMapping("/admin/signup")
+//    public String adminSignup(Model model) {
+//
+//    }
 }
 
