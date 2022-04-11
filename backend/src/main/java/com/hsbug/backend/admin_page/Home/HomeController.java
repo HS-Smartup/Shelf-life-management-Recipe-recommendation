b53210d45519fcbbfea5af149bbf7892897705aa
bear109 <jokerbear109@gmail.com>
2022-04-11 20:37:54 +0900

    fix: 백엔드 통신 수정

--- backend/src/main/java/com/hsbug/backend/admin_page/Home/HomeController.java
+++ backend/src/main/java/com/hsbug/backend/admin_page/Home/HomeController.java
@@ -1,8 +1,13 @@
 package com.hsbug.backend.admin_page.Home;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
 //관리자 페이지 컨트롤러
 public class HomeController {
-    @RequestMapping(value = "/home", method=RequestMethod.GET)
-
+    @RequestMapping(value = "/home", method= RequestMethod.GET)
     public String goHome(HttpServletRequest request) {
         return "content/home";
     }
