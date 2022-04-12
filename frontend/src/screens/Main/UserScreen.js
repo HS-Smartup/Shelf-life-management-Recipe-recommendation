7eef77882e6e2c6c5110d523ecade2c63d8771e0
bear109 <jokerbear109@gmail.com>
2022-04-12 16:34:31 +0900

    feat: UserScreen UI 구현

--- frontend/src/screens/Main/UserScreen.js
+++ frontend/src/screens/Main/UserScreen.js
@@ -1,7 +1,8 @@
-import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {FlatList, Image, Pressable, StyleSheet, Text, View} from 'react-native';
 import React, {useContext} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import CommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {UserNameContext} from 'contexts/UserNameContext';
 
 const UserScreen = () => {
@@ -31,9 +32,87 @@ const UserScreen = () => {
           <Icon name="notifications-none" size={32} color={'#ff8527'} />
         </Pressable>
       </View>
-      <View style={styles.userNameWrapper}>
-        <Text style={styles.userNameText}>{username}</Text>
-        <Text style={styles.userEmailText}>123@naver.com</Text>
+      <View style={styles.listWrapper}>
+        <FlatList
+          data={[{id: 1}]}
+          renderItem={() => (
+            <View style={styles.list}>
+              <Pressable
+                style={styles.userNameWrapper}
+                android_ripple={{color: '#e1e2e3'}}>
+                <View style={styles.userNameTextWrapper}>
+                  <Text style={styles.userNameText}>
+                    {username}
+                    <Text style={styles.userNameInnerText}> 님</Text>
+                  </Text>
+                  <Text style={styles.userEmailText}>123@naver.com</Text>
+                </View>
+                <Icon name="arrow-forward-ios" size={32} color={'#ff8527'} />
+              </Pressable>
+              <View style={styles.recipeBtnWrapper}>
+                <Pressable
+                  style={styles.recipeBtn}
+                  android_ripple={{color: '#e1e2e3'}}>
+                  <CommunityIcon
+                    name="heart-outline"
+                    size={38}
+                    color={'#ff8527'}
+                  />
+                  <Text style={styles.recipeBtnText}>
+                    좋아요 한{`\n\t\t`} 레시피
+                  </Text>
+                </Pressable>
+                <Pressable
+                  style={styles.recipeBtn}
+                  android_ripple={{color: '#e1e2e3'}}>
+                  <Icon name="preview" size={38} color={'#ff8527'} />
+                  <Text style={styles.recipeBtnText}>
+                    최근{`\t`}본{`\n`} 레시피
+                  </Text>
+                </Pressable>
+                <Pressable
+                  style={styles.recipeBtn}
+                  android_ripple={{color: '#e1e2e3'}}>
+                  <CommunityIcon
+                    name="magnify-plus-outline"
+                    size={38}
+                    color={'#ff8527'}
+                  />
+                  <Text style={styles.recipeBtnText}>
+                    내가 작성한{`\n\t\t\t\t`}레시피
+                  </Text>
+                </Pressable>
+              </View>
+              <Pressable
+                style={styles.listItemWrapper}
+                android_ripple={{color: '#e1e2e3'}}>
+                <Text style={styles.listItemText}>공지사항</Text>
+                <Icon name="arrow-forward-ios" size={26} color={'#ffb856'} />
+              </Pressable>
+              <Pressable
+                style={styles.listItemWrapper}
+                android_ripple={{color: '#e1e2e3'}}>
+                <Text style={styles.listItemText}>고객센터</Text>
+                <Icon name="arrow-forward-ios" size={26} color={'#ffb856'} />
+              </Pressable>
+              <Pressable
+                style={styles.listItemWrapper}
+                android_ripple={{color: '#e1e2e3'}}>
+                <Text style={styles.listItemText}>환경설정</Text>
+                <Icon name="arrow-forward-ios" size={26} color={'#ffb856'} />
+              </Pressable>
+              <Pressable
+                style={styles.listItemWrapper}
+                android_ripple={{color: '#e1e2e3'}}>
+                <Text style={styles.listItemText}>약관 및 정책</Text>
+                <Icon name="arrow-forward-ios" size={26} color={'#ffb856'} />
+              </Pressable>
+              <View style={styles.listLastItemWrapper}>
+                <Text style={styles.listItemText}>현재 버전 0.01</Text>
+              </View>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
@@ -78,23 +157,84 @@ const styles = StyleSheet.create({
   notification: {
     marginLeft: 20,
   },
-  userNameWrapper: {
+  listWrapper: {
+    flex: 1,
+  },
+  list: {
     width: '100%',
+    alignItems: 'center',
+  },
+  userNameWrapper: {
+    width: '95%',
     height: 150,
-    paddingLeft: 20,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
     borderBottomColor: '#636773',
     borderBottomWidth: 0.5,
   },
+  userNameTextWrapper: {
+    flexDirection: 'column',
+  },
   userNameText: {
-    fontFamily: 'NanumSquareRoundOTFR',
+    fontFamily: 'NanumSquareRoundOTFB',
     fontSize: 28,
     color: '#000000',
+  },
+  userNameInnerText: {
+    fontFamily: 'NanumSquareRoundOTFR',
+    fontSize: 26,
+    color: '#000',
   },
   userEmailText: {
     fontFamily: 'NanumSquareRoundOTFR',
     fontSize: 18,
     color: '#636773',
     marginTop: 15,
+  },
+  recipeBtnWrapper: {
+    width: '95%',
+    height: 110,
+    flexDirection: 'row',
+    paddingHorizontal: 5,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomColor: '#636773',
+    borderBottomWidth: 0.5,
+  },
+  recipeBtn: {
+    width: '33%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  recipeBtnText: {
+    fontFamily: 'NanumSquareRoundOTFB',
+    fontSize: 16,
+    color: '#000',
+    marginTop: 10,
+  },
+  listItemWrapper: {
+    width: '100%',
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    borderBottomColor: '#636773',
+    borderBottomWidth: 0.5,
+  },
+  listLastItemWrapper: {
+    width: '100%',
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+  },
+  listItemText: {
+    fontFamily: 'NanumSquareRoundOTFB',
+    fontSize: 16,
+    color: '#000',
   },
 });
