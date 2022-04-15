4ab839b125138a8f780275b2391148e6032a0c73
bear109 <jokerbear109@gmail.com>
2022-04-15 18:28:43 +0900

    feat: RecipeScreen UI 구현

---
+++ frontend/src/components/Recipe/RecipeItem.js
@@ -0,0 +1,68 @@
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const RecipeItem = ({recipeName, recipeWriter, recipeView, recipeImage}) => {
+  return (
+    <Pressable style={styles.itemWrapper} android_ripple={{color: '#e1e2e3'}}>
+      <View style={styles.itemTextWrapper}>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.titleText}>{recipeName}</Text>
+          <Text style={styles.writerText}>by {recipeWriter}</Text>
+        </View>
+        <Text style={styles.viewText}>조회수 {recipeView}</Text>
+      </View>
+      <Image
+        source={{uri: `${recipeImage}`}}
+        style={styles.itemImage}
+        resizeMode="cover"
+      />
+    </Pressable>
+  );
+};
+
+export default RecipeItem;
+
+const styles = StyleSheet.create({
+  itemWrapper: {
+    width: '95%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    elevation: 5,
+  },
+  itemTextWrapper: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: '#636773',
+    borderBottomWidth: 0.5,
+    paddingVertical: 10,
+  },
+  titleWrapper: {},
+  titleText: {
+    fontFamily: 'NanumSquareRoundOTFB',
+    fontSize: 22,
+    color: '#000000',
+    marginBottom: 10,
+  },
+  writerText: {
+    fontFamily: 'NanumSquareRoundOTFR',
+    fontSize: 16,
+    color: '#636773',
+  },
+  viewText: {
+    fontFamily: 'NanumSquareRoundOTFR',
+    fontSize: 13,
+    color: '#636773',
+  },
+  itemImage: {
+    width: '100%',
+    height: 250,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+});
--- frontend/src/screens/Main/RecipeScreen.js
+++ frontend/src/screens/Main/RecipeScreen.js
@@ -2,9 +2,46 @@ import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import RecipeItem from 'components/Recipe/RecipeItem';
 
 const RecipeScreen = () => {
   const navigation = useNavigation();
+
+  const recipeItem = [
+    {
+      recipeName: '순두부찌개',
+      recipeWriter: 'SmartUp',
+      recipeView: '2.7만',
+      recipeImage:
+        'https://blog.kakaocdn.net/dn/c6J5mW/btqOMpKvlrE/KuGMNF8VB0eNRokUv8lxZk/img.png',
+    },
+    {
+      recipeName: '제육볶음',
+      recipeWriter: '지나가던 자취생',
+      recipeView: '1.6만',
+      recipeImage:
+        'https://recipe1.ezmember.co.kr/cache/recipe/2019/10/10/04fb8b97fee9072002a0aa54c266a22e1.jpg',
+    },
+    {
+      recipeName: '소고기 미역국',
+      recipeWriter: '스파이더맨',
+      recipeView: '5687',
+      recipeImage:
+        'https://recipe1.ezmember.co.kr/cache/recipe/2015/12/24/7b10402a82606a5a3de6710c93a110f41.jpg',
+    },
+    {
+      recipeName: '닭볶음탕',
+      recipeWriter: '맥북 유저',
+      recipeView: '4586',
+      recipeImage: 'https://t1.daumcdn.net/cfile/tistory/21323847550ECD3D26',
+    },
+    {
+      recipeName: '부대찌개',
+      recipeWriter: '셰프 꿈나무',
+      recipeView: '3281',
+      recipeImage: 'https://t1.daumcdn.net/cfile/tistory/201E751C4C27CF7F50',
+    },
+  ];
 
   return (
     <View style={styles.fullScreen}>
@@ -30,8 +67,17 @@ const RecipeScreen = () => {
       </View>
       <View style={styles.listWrapper}>
         <FlatList
-          data={[{id: 1}]}
-          renderItem={() => <View style={styles.list}></View>}
+          data={recipeItem}
+          renderItem={({item}) => (
+            <View style={styles.list}>
+              <RecipeItem
+                recipeName={item.recipeName}
+                recipeWriter={item.recipeWriter}
+                recipeView={item.recipeView}
+                recipeImage={item.recipeImage}
+              />
+            </View>
+          )}
         />
       </View>
     </View>
@@ -69,5 +115,11 @@ const styles = StyleSheet.create({
   },
   notification: {
     marginLeft: 20,
+  },
+  listWrapper: {
+    flex: 1,
+  },
+  list: {
+    alignItems: 'center',
   },
 });
