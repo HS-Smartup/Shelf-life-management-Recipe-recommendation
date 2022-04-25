53a38fa91dd3d44c17a45c4ccec86ee5411814a1
jungking <immenige2@gmail.com>
2022-04-25 17:58:27 +0900

    feat : thymeleaf content

--- backend/src/main/java/com/hsbug/backend/admin_page/Home/HomeController.java
+++ backend/src/main/java/com/hsbug/backend/admin_page/Home/HomeController.java
@@ -2,6 +2,7 @@ package com.hsbug.backend.admin_page.Home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,4 +20,9 @@ public class HomeController {
     public String login(){
         return "post/login";
     }
+
+
+
+    @GetMapping("/admin/adminManage")
+    public String adminManage(){ return "adminManage";}
 }
