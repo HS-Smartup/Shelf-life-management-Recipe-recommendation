0f3e3e1e8d88987f5e140b9b304e532cfb81c504
jungking <immenige2@gmail.com>
2022-05-12 12:28:33 +0900

    Fix : YOLo 통신

--- backend/src/main/java/com/hsbug/backend/app/search_recipe/_yolo/YoloController.java
+++ backend/src/main/java/com/hsbug/backend/app/search_recipe/_yolo/YoloController.java
@@ -1,6 +1,7 @@
 package com.hsbug.backend.app.search_recipe._yolo;
 
 import com.sun.net.httpserver.HttpsParameters;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +15,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user/search")
 public class YoloController {
 
@@ -29,9 +32,28 @@ public class YoloController {
     @GetMapping("/predict")
     public void Predict(@RequestParam String obj){
         System.out.println(obj);
-        String[] aaa = obj.split(":");
-        System.out.println(Arrays.toString(aaa));
-
+        String[] obj_list = obj.split(":");
+        ArrayList food_obj = new ArrayList<>();
+        for (int i = 0 ; i< obj_list.length; i++){
+            food_obj.add(obj_list[i]);
+        }
+        System.out.println(Arrays.toString(obj_list));
+        food_obj = this.convert(food_obj);
+        System.out.println(food_obj);
         // image file 인수로 같이 넘겨 줘야 함.
+    }
+
+    public ArrayList convert(ArrayList obj_list)throws NullPointerException {
+        ArrayList converted = new ArrayList<String>();
+        for (int i = 0; i < obj_list.size(); i++) {
+            String tmp = (String) obj_list.get(i);
+            String food = null;
+            if (tmp.equals("mandarin")){
+                System.out.println(1);
+            }
+            converted.add(food);
+        }
+
+        return converted;
     }
 }
---
+++ backend/src/main/java/com/hsbug/backend/app/search_recipe/_yolo/YoloService.java
@@ -0,0 +1,13 @@
+package com.hsbug.backend.app.search_recipe._yolo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class YoloService {
+
+    private void findInIngredients(ArrayList obj){
+
+    }
+}
