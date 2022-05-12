73ccf237b32b4bb3fc99cdf0383a07e36e6b4daa
jungking <immenige2@gmail.com>
2022-05-12 11:41:47 +0900

    Fix : YOLo 통신

--- backend/src/main/java/com/hsbug/backend/app/search_recipe/_yolo/YoloController.java
+++ backend/src/main/java/com/hsbug/backend/app/search_recipe/_yolo/YoloController.java
@@ -1,7 +1,9 @@
 package com.hsbug.backend.app.search_recipe._yolo;
 
+import com.sun.net.httpserver.HttpsParameters;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,52 +14,24 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 
 
 @RestController
 @RequestMapping("/user/search")
 public class YoloController {
 
-
-    @GetMapping("/maintest")
-    public String Test(){
-
-        String url = "http://127.0.0.1:5000";
-        String sb = "";
-        try {
-            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-
-            BufferedReader br = new BufferedReader((new InputStreamReader(conn.getInputStream(),"utf-8")));
-
-            String line = null;
-
-            while ((line = br.readLine()) != null) {
-                sb = sb + line + "\n";
-            }
-            System.out.println("========br========" + sb.toString());
-
-            if (sb.toString().contains("ok")){
-                System.out.println("test");
-            }
-            br.close();
-
-            System.out.println("" + sb.toString());
-        } catch(MalformedURLException e){
-            e.printStackTrace();
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-
-        return sb;
-    }
     @GetMapping("/train")
     public void Train(HttpServletResponse httpServletResponse) throws IOException {
         httpServletResponse.sendRedirect("http://localhost:5000/train");
     }
 
     @GetMapping("/predict")
-    public void Predict(HttpServletResponse httpServletResponse) throws IOException {
-        httpServletResponse.sendRedirect("http://localhost:5000/predict");
+    public void Predict(@RequestParam String obj){
+        System.out.println(obj);
+        String[] aaa = obj.split(":");
+        System.out.println(Arrays.toString(aaa));
+
         // image file 인수로 같이 넘겨 줘야 함.
     }
 }
