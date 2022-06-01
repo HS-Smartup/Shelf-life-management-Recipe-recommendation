a7c89e9b6de5d9c211897c6206aa8999e07a038e
bear109 <jokerbear109@gmail.com>
2022-06-01 22:47:26 +0900

    feat: 알림 기능 세팅

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,12 @@
  */
 
 import {AppRegistry} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import App from './App';
 import {name as appName} from './app.json';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
